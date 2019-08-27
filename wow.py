e416574caa7404f3dcbfdc57f7eabe719210d3d5
Blair Harper <blairharper@Blairs-MacBook-Pro.local>
2019-08-27 20:37:40 +0100

    fix indentation

--- wow.py
+++ wow.py
@@ -9,13 +9,13 @@ def main():
     starttime=time.time()
     keys = ['left', 'right', 'up', 'down', 'space']
     while True:
-    currentWindow = GetWindowText(GetForegroundWindow())
-    key = random.randint(0, 4)
-    if(currentWindow == wowTitle):
-        print("Current window is WoW... pressing key: " + keys[key])
-        pyautogui.press(keys[key])
-    interval = random.randint(1, 120)
-    time.sleep(interval - ((time.time() - starttime) % interval))
+        currentWindow = GetWindowText(GetForegroundWindow())
+        key = random.randint(0, 4)
+        if(currentWindow == wowTitle):
+            print("Current window is WoW... pressing key: " + keys[key])
+            pyautogui.press(keys[key])
+        interval = random.randint(1, 120)
+        time.sleep(interval - ((time.time() - starttime) % interval))
 
 if __name__ == "__main__":
     main()
